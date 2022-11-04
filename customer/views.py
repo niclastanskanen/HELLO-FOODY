53a1cd9c869899597160ea75f15766d6c25d15aa
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-04 13:13:27 +0000

    feat: add order calculations for customer

--- customer/views.py
+++ customer/views.py
@@ -45,3 +45,41 @@ class Order(View):
 
         # render the template
         return render(request, 'customer/order.html', context)
+
+    # grab all the selected items
+    # get menu item for that item thats selected
+    # return name, price and id to show calculations price
+    # then pass in the items and price to show in a template afterwards
+    def post(self, request, *args, **kwargs):
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk__contains=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price
+            }
+
+            order_items['items'].append(item_data)
+
+            price = 0
+            item_ids = []
+
+            for item in order_items['items']:
+                price += item['price']
+                item_ids.append(item['id'])
+
+            order = OrderModel.objects.create(price=price)
+            order.items.add(*item_ids)
+
+            context = {
+                'items': order_items['items'],
+                'price': price
+            }
+
+            return render(request, 'customer/order_confirmation.html', context)
