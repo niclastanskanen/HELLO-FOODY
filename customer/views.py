d2f72ebf9fc9e9fdec1ce470ccc970dd2481de3a
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-04 12:54:34 +0000

    feat: add order items to display for customer

--- customer/views.py
+++ customer/views.py
@@ -25,3 +25,23 @@ class Login(View):
 class Cart(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'customer/cart.html')
+
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        starters = MenuItem.objects.filter(category__name__contains='Starters')
+        main = MenuItem.objects.filter(category__name__contains='Main')
+        desserts = MenuItem.objects.filter(category__name__contains='Desserts')
+        drinks = MenuItem.objects.filter(category__name__contains='Drinks')
+
+        # pass into context
+        context = {
+            'starters': starters,
+            'main': main,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+
+        # render the template
+        return render(request, 'customer/order.html', context)
