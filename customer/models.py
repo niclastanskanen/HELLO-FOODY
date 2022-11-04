0548a5a36759b70c996b817acf8dd845f4d9c1e4
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-04 18:37:36 +0000

    feat: add if order is paid in models.py customer templates with migrate

--- customer/models.py
+++ customer/models.py
@@ -29,6 +29,7 @@ class Category(models.Model):
 # and any items connected to that order
 # each menu item can have multiple order
 # each order can have multiplemenu items
+# checking if order is paid
 class OrderModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
@@ -40,6 +41,7 @@ class OrderModel(models.Model):
     city = models.CharField(max_length=50, blank=True)
     zip_code = models.IntegerField(blank=True, null=True)
     phone = models.IntegerField(blank=True, null=True)
+    is_paid = models.BooleanField(default=False)
 
     def __str__(self):
         return f'Order: {self.created_on.strftime("%b %d %I: %M %p")}'
