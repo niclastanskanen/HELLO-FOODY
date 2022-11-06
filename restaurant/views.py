16e02b5f536fca7dcda274034e4f094ab9b5b5ff
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-06 17:50:02 +0000

    add restaurant statistics in dashboard

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+from django.utils.timezone import datetime
+from customer.models import OrderModel
 
 
-class Dashboard(View):
+class Dashboard(LoginRequired, UserPassesTest, View):
     def get(self, request, *args, **kwargs):
+        # get the current date
+        today = datetime.today()
+        orders = OrderModel.objects.filter(
+            created_on__year=today.year,
+            created_on__month=today.month,
+            created_on__day=today.day)
 
-        return render(request, 'restaurant/dashboard.html')
+        # loop throught the orders and add the price value
+        total_revenue = 0
+        for order in orders:
+            total_revenue += order.price
+
+        # pass total number of orders and total revenue into template
+        context = {
+            'orders': orders,
+            'total_revenue': total_revenue,
+            'total_orders': len(orders)
+        }
+
+        return render(request, 'restaurant/dashboard.html', context)
 
