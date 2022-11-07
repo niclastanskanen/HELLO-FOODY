14af93c74b35c72efa713d081144c16827e94762
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-07 10:19:05 +0000

    add order details for restaurants logged in

--- restaurant/views.py
+++ restaurant/views.py
@@ -32,3 +32,16 @@ class Dashboard(LoginRequiredMixin, UserPassesTestMixin, View):
     def test_func(self):
         return self.request.user.groups.filter(name='Staff').exists()
 
+
+# Order details for restaurang logged in
+class OrderDetails(LoginRequiredMixin, UserPassesTestMixin, View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+        context = {
+            'order': order
+        }
+
+        return render(request, 'restaurant/order-details.html', context)
+
+    def test_func(self):
+        return self.request.user.groups.filter(name='Staff').exists()
