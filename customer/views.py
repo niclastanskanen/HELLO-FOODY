8a20d6cc83fd2108dd3b3a076c4a7d109baadd3a
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-08 07:53:59 +0000

    add menu search

--- customer/views.py
+++ customer/views.py
@@ -1,6 +1,7 @@
 import json
 from django.shortcuts import render, redirect
 from django.views import View
+from django.db.models import Q
 from django.core.mail import send_mail
 from .models import MenuItem, Category, OrderModel
 
@@ -28,7 +29,19 @@ class Menu(View):
 
 class MenuSearch(View):
     def get(self, request, *args, **kwargs):
-        return render(request, 'customer/search.html')
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
 
 
 class Login(View):
