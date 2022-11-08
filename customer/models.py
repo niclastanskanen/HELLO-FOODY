f9d99210762fb77ebcd16d9c39a734fba6d23909
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-08 12:18:59 +0000

    add image to restaurant model

--- customer/models.py
+++ customer/models.py
@@ -5,6 +5,7 @@ from cloudinary.models import CloudinaryField
 # # restaurants
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
+    restaurant_image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.name
