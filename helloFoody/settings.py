66b0da8a9bc1f9350b76b8ed4dc5bea0bcc92ebb
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-14 14:31:20 +0000

    fix testcase for testing

--- helloFoody/settings.py
+++ helloFoody/settings.py
@@ -135,9 +135,18 @@ WSGI_APPLICATION = 'helloFoody.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'TESTING' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+# otherwise, connect to Postgres DB
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 
 
 # Password validation
