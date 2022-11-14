237bb3ba8d9ad1343b674c64b3ace1c163baaf8c
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-14 12:18:36 +0000

    add links to download app buttons

--- helloFoody/settings.py
+++ helloFoody/settings.py
@@ -31,7 +31,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
 
 ALLOWED_HOSTS = ['hello-foody.herokuapp.com', 'localhost']
 
