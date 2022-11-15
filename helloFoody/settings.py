3cc2bf50565c8f8eb1bd595fc7c2021f6d89950e
niclastanskanen <niclastanskanen@hotmail.com>
2022-11-15 17:15:25 +0000

    deployment commit

--- helloFoody/settings.py
+++ helloFoody/settings.py
@@ -188,6 +188,3 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-CSRF_TRUSTED_ORIGINS = ['https://8000-niclastanska-hellofoody-zbjlc6wf5qo.ws-eu74.gitpod.io']
